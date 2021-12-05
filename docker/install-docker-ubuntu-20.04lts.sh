7bc3baa9cfa49026f81ffdd10c7df378275804a2
nord73 <1984553+nord73@users.noreply.github.com>
2021-12-06 00:01:00 +0100

    Update install-docker-ubuntu-20.04lts.sh

--- docker/install-docker-ubuntu-20.04lts.sh
+++ docker/install-docker-ubuntu-20.04lts.sh
@@ -25,5 +25,5 @@ newgrp docker
 sudo apt install python3-pip -y
 sudo apt install virtualenv -y
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/2.2.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
