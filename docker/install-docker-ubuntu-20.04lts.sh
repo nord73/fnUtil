a7f11fed584624f316e87c86635e54844b85e1e7
nord73 <1984553+nord73@users.noreply.github.com>
2022-02-04 00:37:06 +0100

    .

--- docker/install-docker-ubuntu-20.04lts.sh
+++ docker/install-docker-ubuntu-20.04lts.sh
@@ -24,7 +24,7 @@ newgrp docker
 
 sudo apt install python3-pip -y
 sudo apt install virtualenv -y
-sudo apt install vim-nox -y
+# sudo apt install vim-nox -y
 
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
